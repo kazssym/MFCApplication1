a330fc7dac71bf04dadc012784ce2cfc468629ce
Kaz Nishimura <kazssym@linuxfront.com>
2024-02-05 22:41:16 +0900

    Include C++/WinRT headers

--- MFCApplication1/framework.h
+++ MFCApplication1/framework.h
@@ -29,6 +29,32 @@
 #include <afxcontrolbars.h>     // MFC support for ribbons and control bars
 
 
+#pragma push_macro("GetCurrentTime")
+#undef GetCurrentTime
+
+#pragma push_macro("TRY")
+#undef TRY
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Microsoft.UI.Composition.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Data.h>
+#include <winrt/Microsoft.UI.Xaml.Interop.h>
+#include <winrt/Microsoft.UI.Xaml.Markup.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.UI.Xaml.Navigation.h>
+#include <winrt/Microsoft.UI.Xaml.Shapes.h>
+#include <winrt/Microsoft.UI.Dispatching.h>
+
+#pragma pop_macro("TRY")
+
+#pragma pop_macro("GetCurrentTime")
+
+
 
 
 
